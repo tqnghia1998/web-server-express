a98a6441329e53965a621d157efa5a77086c1b4e
Android-Nhom15 <xuanghjem@gmail.com>
2019-06-16 23:30:08 +0700

    Xong quản lý bài viết

--- ENDTERM/models/posts.model.js
+++ ENDTERM/models/posts.model.js
@@ -19,6 +19,10 @@ module.exports = {
         return db.update(`posts`, 'posID', entity);
     },
 
+    delete: id => {
+        return db.delete(`posts`, 'posID', id);
+    },
+
     allWaitingEditor: idEditor => {
         return db.load(`select *, date_format(DayWritten,"%d-%m-%Y") as DayWrittenFormat from posts p join editors e on p.cateID = e.cateID join categories c on p.cateID = c.cateID where userID = ${idEditor} and Approved = 0 and Additional = ''`);
     },
@@ -26,30 +30,30 @@ module.exports = {
     allPublish: idWriter => {
         return db.load(`select * from posts where (DayPublish < Date(now()) or Published = 1) and Writer = ${idWriter}`)
     },
-    
+
     // NghiaTQ
     mostViewedInWeek: (monday) => {
         return db.load(`SELECT posID, Title, Description, Views FROM posts WHERE DATE(DayPublish) >= '${monday}' ORDER BY Views DESC LIMIT 4`);
     },
     mostViewed: () => {
         var sqlQuery = "SELECT posID, Title, Views, Description, child.cateName as child, parent.cateName as parent "
-                        + "FROM posts INNER JOIN categories as child ON posts.cateID = child.cateID "
-                        + "LEFT JOIN categories as parent ON child.parentID = parent.cateID ORDER BY Views DESC LIMIT 10";
+            + "FROM posts INNER JOIN categories as child ON posts.cateID = child.cateID "
+            + "LEFT JOIN categories as parent ON child.parentID = parent.cateID ORDER BY Views DESC LIMIT 10";
         return db.load(sqlQuery);
     },
     mostRecent: () => {
         var sqlQuery = "SELECT posID, Title, DayPublish, Description, child.cateName as child, parent.cateName as parent "
-                        + "FROM posts INNER JOIN categories as child ON posts.cateID = child.cateID "
-                        + "LEFT JOIN categories as parent ON child.parentID = parent.cateID ORDER BY DayPublish DESC LIMIT 10"
+            + "FROM posts INNER JOIN categories as child ON posts.cateID = child.cateID "
+            + "LEFT JOIN categories as parent ON child.parentID = parent.cateID ORDER BY DayPublish DESC LIMIT 10"
         return db.load(sqlQuery);
     },
     topCategories: () => {
         var sqlQuery = "SELECT child.cateName as child, parent.cateName as parent, posts.DayPublish, posts.Title, posts.posID "
-                        + "FROM categories as child LEFT JOIN categories as parent ON child.parentID = parent.cateID "
-                        + "LEFT JOIN posts ON posts.cateID = child.cateID WHERE DayPublish = ("
-                        + "SELECT MAX(DayPublish) "
-                        + "FROM posts "
-                        + "WHERE posts.cateID = child.cateID) LIMIT 10";
+            + "FROM categories as child LEFT JOIN categories as parent ON child.parentID = parent.cateID "
+            + "LEFT JOIN posts ON posts.cateID = child.cateID WHERE DayPublish = ("
+            + "SELECT MAX(DayPublish) "
+            + "FROM posts "
+            + "WHERE posts.cateID = child.cateID) LIMIT 10";
         return db.load(sqlQuery);
     },
     countPostByCate: (cateID) => {
@@ -57,27 +61,27 @@ module.exports = {
     },
     countPostByTag: (tagName) => {
         var sqlQuery = `SELECT COUNT(*) AS total FROM baodientu16th.posts INNER JOIN baodientu16th.postsandtags `
-                        + `ON posts.posID = postsandtags.posID INNER JOIN baodientu16th.tags `
-                        + `ON postsandtags.tagID = tags.tagID AND tags.tagName = '${tagName}'`
+            + `ON posts.posID = postsandtags.posID INNER JOIN baodientu16th.tags `
+            + `ON postsandtags.tagID = tags.tagID AND tags.tagName = '${tagName}'`
         return db.load(sqlQuery);
     },
     postsByCate: (cateID, limit, offset) => {
         var sqlQuery = `SELECT posID, Title, DayPublish, Description, child.cateName as child, parent.cateName as parent `
-                        + `FROM posts INNER JOIN categories as child ON posts.cateID IN ( `
-                        + `SELECT cateID `
-                        + `FROM categories c `
-                        + `WHERE c.parentID = ${cateID} OR c.cateID = ${cateID})`
-                        + `AND posts.cateID = child.cateID `
-                        + `LEFT JOIN categories as parent ON child.parentID = parent.cateID LIMIT ${limit} OFFSET ${offset}`;
+            + `FROM posts INNER JOIN categories as child ON posts.cateID IN ( `
+            + `SELECT cateID `
+            + `FROM categories c `
+            + `WHERE c.parentID = ${cateID} OR c.cateID = ${cateID})`
+            + `AND posts.cateID = child.cateID `
+            + `LEFT JOIN categories as parent ON child.parentID = parent.cateID LIMIT ${limit} OFFSET ${offset}`;
         return db.load(sqlQuery);
     },
     postsByTag: (tagName, limit, offset) => {
         var sqlQuery = `SELECT posts.posID, Title, DayPublish, Description, child.cateName as child, parent.cateName as parent `
-                        + `FROM posts INNER JOIN postsandtags `
-                        + `ON posts.posID = postsandtags.posID INNER JOIN tags ON postsandtags.tagID = tags.tagID AND tags.tagName = '${tagName}' `
-                        + `LEFT JOIN categories as child ON posts.cateID = child.cateID `
-                        + `LEFT JOIN categories as parent ON child.parentID = parent.cateID LIMIT ${limit} OFFSET ${offset}`;
-        return db.load(sqlQuery);              
+            + `FROM posts INNER JOIN postsandtags `
+            + `ON posts.posID = postsandtags.posID INNER JOIN tags ON postsandtags.tagID = tags.tagID AND tags.tagName = '${tagName}' `
+            + `LEFT JOIN categories as child ON posts.cateID = child.cateID `
+            + `LEFT JOIN categories as parent ON child.parentID = parent.cateID LIMIT ${limit} OFFSET ${offset}`;
+        return db.load(sqlQuery);
     },
 
     allApproved: idWriter => {
@@ -87,7 +91,7 @@ module.exports = {
     allWaiting: idWriter => {
         return db.load(`select *, date_format(DayWritten,"%d-%m-%Y") as DayWrittenFormat from posts p join categories c on p.cateID = c.cateID where Approved = 0 and Additional = '' and Writer = ${idWriter}`);
     },
-    
+
     allReject: idWriter => {
         return db.load(`select * from posts where Approved = 0 and Additional <> '' and Writer = ${idWriter}`);
     },
--- ENDTERM/routes/admin/post.route.js
+++ ENDTERM/routes/admin/post.route.js
@@ -3,23 +3,54 @@ var Model = require('../../models/posts.model');
 
 var router = express.Router();
 
-router.get('/', (req, res)=>{
+router.get('/', (req, res) => {
     var u = Model.allbycate();
-    
-    u.then(rows=>{
+    u.then(rows => {
         console.log(rows);
-        res.render('page/admin/vwposts/post',{
+        for (i = 0; i < rows.length; i++) {
+            var Day = rows[i].DayPublish;
+            Day.setMinutes(Day.getMinutes() - Day.getTimezoneOffset());
+            if (Day.toISOString().slice(0, 10) > new Date().toISOString().slice(0, 10) || rows[i].Published) {
+                rows[i].isPublished = true;
+            }
+            else rows[i].isPublished = false;
+        }
+        res.render('page/admin/vwposts/post', {
             layout: 'admin',
             posts: rows
         });
-    }).catch(error=>{
+    }).catch(error => {
         console.log(error);
     });
     //res.end('lafm bieesn quas')
-    
+
 });
-router.get('/viewpost', (req, res)=>{
-    res.render('page/admin/vwposts/viewpost',{layout: 'admin'});
+
+router.post('/publish/:id', (req, res) => {
+    var id = req.params.id;
+    var p = Model.single(id);
+    p.then(rows => {
+        if (rows.length > 0) {
+            var post = rows[0];
+            var entity = {
+                posID: post.posID,
+                Published: 1,
+            }
+        }
+        Model.update(entity).then(n => {
+            res.redirect('/admin/post')
+        })
+    }).catch(error => {
+        console.log(error);
+    });
 })
 
+router.post('/delete/:id', (req, res) => {
+    var id = req.params.id;
+    Model.delete(id).then(n => {
+        res.redirect('/admin/post');
+    }).catch(error => {
+        console.log(error);
+    });
+})
 module.exports = router;
